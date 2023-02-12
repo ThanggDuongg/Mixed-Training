139cc976ba9915cb2b45239888d003a3c27f0018
ThanggDuongg <thangduc.duong14@gmail.com>
2023-02-12 15:47:03 +0700

    feat: leetcode sqrt(x)

---
+++ Leetcode/Leetcode.Sqrt(x)/Program.cs
@@ -0,0 +1,28 @@
+﻿Solution solution = new Solution();
+Console.WriteLine(solution.MySqrt(2147395600));
+
+public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x == 0)
+        {
+            return 0;
+        }
+        else if (x == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            long i = 1;
+            long flag = i * i;
+            while ((long)x >= flag)
+            {
+                i++;
+                flag = i * i;
+            }
+            return (int)(i - 1);
+        }
+    }
+}
